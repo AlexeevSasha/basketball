37641d22b42c70d8907f379ac7c4219b433a47ee
AlexeevSasha <work.alexeevs@gmail.com>
2022-05-02 12:07:52 +0300

    export

---
+++ src/common/components/index.tsx
@@ -0,0 +1,24 @@
+export {Button} from './Button/Button';
+export {ButtonCancel} from './Button/ButtonCancel';
+export {Header} from './Header/Header';
+export {HeaderCardInfo} from './HeaderCardInfo/HeaderCardInfo';
+export {Input} from './Input/Input';
+export {InputCheckbox} from './Input/InputCheckbox';
+export {InputFile} from './Input/InputFile'
+export {Search} from './Input/Search';
+export {NavMenu} from './NavMenu/NavMenu';
+export {Pagination} from './Pagination/Pagination';
+export {Selects} from './Select/Select';
+export {PopapDelete} from './PopapDelete/PopapDelete'
+export { Empty} from  './Empty/Empty'
+export {Spinner} from './Spinner/Spinner'
+export {Notification} from './Notification/Notification'
+export {BreadCrumbs} from './BreadCrumbs/BreadCrumbs'
+
+
+//icons / imageSvg
+export {ChekMarkIcon, ProfileIcon, ArrowRigthIcon, ArrowLeftIcon,
+        CreateIcon, DeleteIcon, EyeIcon,  MenuIcon, CloseEyeIcon,
+        PlayersIcon, SearchIcon, SingOutIcon, TeamsIcon, InputFileIcon,
+        Error404Svg, EmptyTeamsSvg, SingInSvg, SingUpSvg, EmptyPlayersSvg
+        }from './SVGConstans/SVG'
